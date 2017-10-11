96f0958caca792d1659cfeb103766029990d412e
Francisca Cerquinho <francisca@MBPdeFrancisca.lan>
2017-10-11 23:02:21 +0100

    alteracoes

--- EstadoInicialTabuleiro.pl
+++ EstadoInicialTabuleiro.pl
@@ -2,7 +2,7 @@ boards(initialBoard,[piece1,empty,piece1,empty,piece1,empty,piece1],
 [empty,piece1,empty,piece1,empty,piece1,empty],
 [empty,empty,piece1,empty,piece1,empty,empty],
 [empty,empty,empty,noPiece,empty,empty,empty],
-[empty,empty,noPiece,empty,noPiece,empty,empty],
+[empty,empty,noPiece,noPiece,noPiece,empty,empty],
 [empty,empty,empty,noPiece,empty,empty,empty],
 [empty,empty,piece2,empty,piece2,empty,empty],
 [empty,piece2,empty,piece2,empty,piece2,empty],
@@ -13,7 +13,7 @@ boards(gameBoard,
 [empty,noPiece,empty,noPiece,empty,noPiece,empty],
 [empty,empty,piece1,empty,noPiece,empty,empty],
 [empty,empty,empty,piece1,empty,empty,empty],
-[empty,empty,noPiece,empty,piece2,empty,empty],
+[empty,empty,noPiece,noPiece,piece2,empty,empty],
 [empty,empty,empty,noPiece,empty,empty,empty],
 [empty,empty,noPiece,empty,piece2,empty,empty],
 [empty,noPiece,empty,piece2,empty,noPiece,empty],
@@ -25,8 +25,16 @@ boards(finalBoard,
 [empty,noPiece,empty,noPiece,empty,noPiece,empty],
 [empty,empty,noPiece,empty,noPiece,empty,empty],
 [empty,empty,empty,noPiece,empty,empty,empty],
-[empty,empty,noPiece,empty,noPiece,empty,empty],
+[empty,empty,noPiece,noPiece,noPiece,empty,empty],
 [empty,empty,empty,noPiece,empty,empty,empty],
 [empty,empty,noPiece,empty,piece2,empty,empty],
 [empty,noPiece,empty,piece2,empty,noPiece,empty],
 [piece2,empty,piece2,empty,piece2,empty,piece2]]).
+
+printLetters:-
+write('A     B     C     D     E     F     G     H    I').
+printLine:-
+write('________________________________________________').
+
+printNumbers([' 1 ', ' 2 ',' 3 ', ' 4 ',' 5 ',' 6 ',' 7 ', ' 8 ',' 9 ']).
+printRowBoard([], []).
