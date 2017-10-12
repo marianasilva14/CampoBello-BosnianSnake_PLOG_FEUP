02a92dd989e965238d247b27f9767ccd173d5b36
Francisca Cerquinho <francisca@wifi-interno.fe.up.pt>
2017-10-12 16:02:59 +0100

    board almost done

--- EstadoInicialTabuleiro.pl
+++ EstadoInicialTabuleiro.pl
@@ -1,51 +1,34 @@
 getSymbol(empty, ' ').
 getSymbol(piece1, 'X').
 getSymbol(piece2, 'Y').
-getSymbol(noPiece, 'NP').
+getSymbol(noPiece, 'N').
 
 
 initialBoard([
-[piece1,empty,piece1,empty,piece1,empty,piece1],
-[empty,piece1,empty,piece1,empty,piece1,empty],
-[empty,empty,piece1,empty,piece1,empty,empty],
-[empty,empty,empty,noPiece,empty,empty,empty],
-[empty,empty,noPiece,noPiece,noPiece,empty,empty],
-[empty,empty,empty,noPiece,empty,empty,empty],
-[empty,empty,piece2,empty,piece2,empty,empty],
-[empty,piece2,empty,piece2,empty,piece2,empty],
-[piece2,empty,piece2,empty,piece2,empty,piece2]]).
+[empty,piece1,piece1,piece1,piece1,empty,empty,empty,empty],
+[empty,empty,piece1,piece1,piece1,empty,empty,empty,piece2],
+[empty,empty,empty,piece1,piece1,empty,empty,piece2,piece2],
+[empty,empty,empty,empty,noPiece,empty,piece2,piece2,piece2],
+[piece1,piece1,piece1,noPiece,noPiece,noPiece,piece2,piece2,piece2],
+[piece1,piece1,piece1,empty,noPiece,empty,empty,empty,empty],
+[piece1,piece1,empty,empty,piece2,piece2,empty,empty,empty],
+[piece1,empty,empty,empty,piece2,piece2,piece2,empty,empty],
+[empty,empty,empty,empty,piece2,piece2,piece2,piece2,empty]]).
 
-gameBoard([
-[piece1,empty,noPiece,empty,piece2,empty,noPiece],
-[empty,noPiece,empty,noPiece,empty,noPiece,empty],
-[empty,empty,piece1,empty,noPiece,empty,empty],
-[empty,empty,empty,piece1,empty,empty,empty],
-[empty,empty,noPiece,noPiece,piece2,empty,empty],
-[empty,empty,empty,noPiece,empty,empty,empty],
-[empty,empty,noPiece,empty,piece2,empty,empty],
-[empty,noPiece,empty,piece2,empty,noPiece,empty],
-[piece2,empty,piece2,empty,piece2,empty,piece2]]).
-
-
-finalBoard([
-[noPiece,empty,noPiece,empty,noPiece,empty,noPiece],
-[empty,noPiece,empty,noPiece,empty,noPiece,empty],
-[empty,empty,noPiece,empty,noPiece,empty,empty],
-[empty,empty,empty,noPiece,empty,empty,empty],
-[empty,empty,noPiece,noPiece,noPiece,empty,empty],
-[empty,empty,empty,noPiece,empty,empty,empty],
-[empty,empty,noPiece,empty,piece2,empty,empty],
-[empty,noPiece,empty,piece2,empty,noPiece,empty],
-[piece2,empty,piece2,empty,piece2,empty,piece2]]).
-
-
+printFinalBoard([L|Ls]):-
+    printBoard([L|Ls]),
+    printLine.
 printBoard([L|Ls]) :-
-          printRow(L),nl,
+          printLine,nl,
+          printFinalRow(L),nl,
           printBoard(Ls).
-printBoard([]):-
-          nl.
+printBoard([]).
+
+printFinalRow([X|Xs]):-
+        write('|'), printRow([X|Xs]).
 printRow([X|Xs]):-
         getSymbol(X,Piece),
-        write(' '), write(Piece),write('  |'),
+        write(' '), write(Piece),write('  | '),
         printRow(Xs).
+printLine:-write('-----------------------------------------------------').
 printRow([]).
