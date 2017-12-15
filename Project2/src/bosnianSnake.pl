41af078ef540080bea23fde1390ead344e2d3a10
FranciscaCerquinho <franciscacerquinho@gmail.com>
2017-12-15 12:56:43 +0000

    aula update

--- Project2/src/bosnianSnake.pl
+++ Project2/src/bosnianSnake.pl
@@ -1,17 +1,25 @@
 :- use_module(library(clpfd)).
 :- use_module(library(random)).
 :- use_module(library(lists)).
+:- include('continuity.pl').
 
-bosnianSnake :-
-  board(6, 6, BoardIn),
-  setPiece(BoardIn, 3, 5, '6', Board),
-  setPiece(Board, 4, 2, '6', BoardOut),
+puz(1, [1-1, 6-6], 6-6, [2-2,5-1], [], [3-5-6, 4-2-6]).
+
+
+bosnianSnake(N) :-
+  puz(N, [BeginRow-BeginCol,EndRow-EndCol],NR-NC, [RRow-RCol,RRow2-RCol2], [], [IntRow-IntCol-Number,IntRow2-IntCol2-Number2]),
+  write(IntRow),
+  board(NR, NC, Board),
   nl, write('Board'),nl,
-  write(BoardOut),
+  write(Board),
   nl,nl,nl,
   write('Celulas a volta do 6'), nl,
-  getCellsAround(BoardOut, ListOut, 3, 5),
-  write(ListOut).
+  getCellsAround(Board, ListOut, IntRow, IntCol),
+  write(ListOut),
+  nl, nl, nl,
+  write('Celulas c/ restricao'), nl,
+  getCellsOfRestrictionOut(Board,RRow,NR,[],ListOut2),
+  write(ListOut2).
 
 getCellsAround(Board, ListOut, Nrow, Ncol) :-
   FinalRow1 is (Nrow + 1),
@@ -19,18 +27,30 @@ getCellsAround(Board, ListOut, Nrow, Ncol) :-
   FinalCol1 is (Ncol + 1),
   FinalCol2 is (Ncol - 1),
   setof(Element,
-                (getElement(Board,FinalRow1,Ncol,Element),
-                getElement(Board,Nrow,FinalCol1,Element),
-                getElement(Board,FinalRow1,FinalCol1,Element),
-                getElement(Board,FinalRow2,Ncol,Element),
-                getElement(Board,Nrow,FinalCol2,Element),
-                getElement(Board,FinalRow2,FinalCol2,Element),
-                getElement(Board,FinalRow1,FinalCol2,Element),
+                (getElement(Board,FinalRow1,Ncol,Element);
+                getElement(Board,Nrow,FinalCol1,Element);
+                getElement(Board,FinalRow1,FinalCol1,Element);
+                getElement(Board,FinalRow2,Ncol,Element);
+                getElement(Board,Nrow,FinalCol2,Element);
+                getElement(Board,FinalRow2,FinalCol2,Element);
+                getElement(Board,FinalRow1,FinalCol2,Element);
                 getElement(Board,FinalRow2,FinalCol1,Element)),ListOut).
 
 getElement(Board,Nrow,Ncol,Element) :-
   nth1(Nrow, Board, Row),
   nth1(Ncol,Row,Element).
+
+getCellsOfRestrictionOut(_,_,0,L,L).
+getCellsOfRestrictionOut(Board,Row,Size,ListIn,ListOut):-
+  getElement(Board,Row,Size,Element),
+  append([Element],ListIn,ListAux),
+  FinalSize is Size-1,
+  getCellsOfRestrictionOut(Board,Row,FinalSize,ListAux,ListOut).
+
+cellsOfRestrictionOut(Board,Number,Row,Size) :-
+  Number2 is (Size - Number),
+  getCellsOfRestrictionOut(Board,Row,Size,[],ListOut),
+  global_cardinality(ListOut,[1-Number],[0-Number2]).
 
 cellsAround(Board, Nrow, Ncol, Number) :-
   Number2 is (8 - Number),
---
+++ Project2/src/utilities.pl
@@ -0,0 +1,1 @@
+execute:-reconsult('/Users/francisca/Documents/FEUP/3 ano/1 semestre/PLOG/feup-PLOG/Project2/src/bosnianSnake.pl').
