29da0a52fddcaad7fdacc3fe6dc476a03f6db2f4
FranciscaCerquinho <franciscacerquinho@gmail.com>
2017-12-15 18:34:23 +0000

    UPDATE

--- Project2/src/conectivity.pl
+++ Project2/src/conectivity.pl
@@ -19,14 +19,20 @@ checkConectivity(Board,Row,Col,Dim) :-
   getPosition(Dim,Row,C2,Position3),
   P is Position+1,
   P2 is Position-1,
-  if_then_else(Col=Dim,ValuePosition is 0,element(P,List,ValuePosition)),
-  if_then_else(Col=1,ValuePosition2 is 0,element(P2,List,ValuePosition2)),
-  if_then_else(Row=1,ValuePosition4 is 0, element(Position3,List,ValuePosition4)),
-  if_then_else(Row=Dim,ValuePosition3 is 0,element(Position2,List,ValuePosition3)),
-  element(Position, List, ValuePosition5),
-  Value #= ValuePosition+ValuePosition2+ValuePosition3+ValuePosition4,
-  Dim2 is Dim*Dim, trace,
-  if_then_else((Position = 1; Position = Dim2),ValuePosition5 #= 1, ((ValuePosition5 #= 1) #=> (Value#=2))),
-  Row2 is Row+1,
+  P3 is Position+Dim,
+  P4 is Position mod Dim,
+  Dim2 is Dim*Dim,
+  element(Position, List, Value),
+  element(P, List, ValuePosition),
+  element(P2,List,ValuePosition2),
+  element(P3,List,ValuePosition3),
+  element(Position3,List,ValuePosition4),
+  element(Position2,List,ValuePosition3),
+  ((Position #= 1 #/\ (Value #=1 #/\ (ValuePosition #= 1 #\/ ValuePosition3#=1))) #\/
+  (Position #= Dim2 #/\ Value#=1) #\/
+  (1#=P4  #/\ (ValuePosition #= 1  #\/ ValuePosition4 #= 1 #\/ ValuePosition3#=1)) #\/
+  (0#=P4  #/\ (ValuePosition2 #= 1 #\/ ValuePosition4#=1 #\/ValuePosition3#=1)) #\/
+  ((0#\=P4 #/\ 1#\=P4), (ValuePosition=1 #\/ ValuePosition2#=1 #\/ ValuePosition3#=1 #\/ ValuePosition4#=1))),
   Col2 is Col+1,
+  Row2 is Row+1,
   if_then_else(Row=Dim,checkConectivity(Board,1,Col2,Dim),checkConectivity(Board,Row2,Col,Dim)).
