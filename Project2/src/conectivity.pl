4797d360328623f2520a3bb74a2bb6b3ee09c47c
FranciscaCerquinho <franciscacerquinho@gmail.com>
2017-12-17 11:43:17 +0000

    update

--- Project2/src/conectivity.pl
+++ Project2/src/conectivity.pl
@@ -1,5 +1,8 @@
 :- use_module(library(clpfd)).
 :- use_module(library(lists)).
+%Predicate that does an if than else
+if_then_else(If, Then,_):- If,!, Then.
+if_then_else(_, _, Else):- Else.
 
 matrixToListOfLists(Board,List) :-
 append(Board,List).
@@ -7,63 +10,61 @@ append(Board,List).
 getPosition(Dim,Row,Col,Position) :-
 Position is (Row-1)*Dim+Col.
 
-checkConectivity(_,Aux,_,_,Aux).
+checkConectivity(_,_,0).
 
-checkConectivity(Board,Row,Dim,Dim,Aux):-
-  Row2 is Row+1,
-  checkConectivity(Board,Row2,1,Dim,Aux).
-
-checkConectivity(Board,Row,Col,Dim,Aux) :-
+checkConectivity(Board,Dim,Position) :-
 matrixToListOfLists(Board,List),
-getPosition(Dim,Row,Col,Position),
-valuePosition(Position,List,Dim),
-write(Position),nl,nl,
-Col2 is Col+1,
-checkConectivity(Board,Row,Col2,Dim,Aux).
+valuePosition(Position,List,Dim,ReturnPosition),
+write(ReturnPosition),nl,nl,
+checkConectivity(Board,Dim,ReturnPosition).
 
 
 %First left position
-valuePosition(Position,List,Size):-
+valuePosition(Position,List,Size,R):-
 Position==1,
 PositionRight is Position+1,
 PositionDown is Position+Size,
 nth1(Position,List,Element),
 nth1(PositionRight,List,ElementRight),
 nth1(PositionDown,List,ElementDown),
-Element#=1  #/\
-((ElementRight#=1) #\/ (ElementDown#=1)).
+Element=1,
+((ElementRight#=1) #\/ (ElementDown#=1)),
+if_then_else(ElementRight==1,R is PositionRight,R is PositionDown).
+
 
 %First right position
-valuePosition(Position,List,Size):-
+valuePosition(Position,List,Size,R):-
 Position==Size,
 PositionLeft is Position-1,
 PositionDown is Position+Size,
-nth1(Position,List,Element),
 nth1(PositionLeft,List,ElementLeft),
 nth1(PositionDown,List,ElementDown),
-Element#=1  #/\ ((ElementLeft#=1) #\/ (ElementDown#=1)).
+((ElementLeft#=1 #<=> B #/\ (B#=0  #/\ ElementLeft#=1)) #\/ (ElementDown#=1 #<=>B #/\ (B#=0  #/\ ElementDown#=1))),
+if_then_else(ElementLeft==1,R is PositionLeft,R is PositionDown).
+
 
 %Last second position
-valuePosition(Position,List,Size):-
+valuePosition(Position,List,Size,ReturnPosition):-
 Dim is Size*Size,
 Position==Dim,
 nth1(Position,List,Element),
-Element#=1.
+Element=1,
+ReturnPosition=0.
 
 %Last first position
-valuePosition(Position,List,Size):-
+valuePosition(Position,List,Size,R):-
 Dim is Size*Size,
 Value is Dim-Size,
 Position==Value,
 PositionRight is Position+1,
 PositionUp is Position-Size,
-nth1(Position,List,Element),
 nth1(PositionRight,List,ElementRight),
 nth1(PositionUp,List,ElementUp),
-Element#=1 #/\ ((ElementRight#=1) #\/ (ElementUp#=1)).
+((ElementRight#\=0  #/\ ElementRight#=1) #\/ (ElementUp#\=0 #/\ ElementUp#=1)),
+if_then_else(ElementRight==1,R is PositionRight,R is PositionUp).
 
 %Left extreme
-valuePosition(Position,List,Size):-
+valuePosition(Position,List,Size,R):-
 Position\=1,
 Dim is Size*Size,
 Value is Dim-Size,
@@ -73,14 +74,24 @@ PositionMod==1,
 PositionRight is Position+1,
 PositionDown is Position+Size,
 PositionUp is Position-Size,
-nth1(Position,List,Element),
 nth1(PositionRight,List,ElementRight),
 nth1(PositionDown,List,ElementDown),
 nth1(PositionUp,List,ElementUp),
-Element#=1  #/\((ElementRight#=1) #\/ (ElementDown#=1) #\/ (ElementUp#=1)).
+write('ElementUp'+ElementUp),nl,
+write('Position'+Position),nl,
+trace,
+((ElementUp == 1,
+(ElementDown #=1  #\/ ElementRight#=1));
+(ElementDown == 1,
+(ElementUp #=1  #\/ ElementRight#=1));
+(ElementRight==1,
+(ElementDown #=1  #\/ ElementUp#=1))),
+write('AQUIIII'),
+if_then_else(ElementRight==1,R is PositionRight,if_then_else(ElementDown==1,R is PositionDown,R is PositionUp)),
+write('R'+R),nl.
 
 %Right extreme
-valuePosition(Position,List,Size):-
+valuePosition(Position,List,Size,R):-
 Position\=Size,
 Dim is Size*Size,
 Position \= Dim,
@@ -89,14 +100,20 @@ PositionMod==0,
 PositionLeft is Position-1,
 PositionDown is Position+Size,
 PositionUp is Position-Size,
-nth1(Position,List,Element),
 nth1(PositionLeft,List,ElementLeft),
 nth1(PositionDown,List,ElementDown),
 nth1(PositionUp,List,ElementUp),
-Element#=1  #/\ ((ElementLeft#=1) #\/ (ElementDown#=1) #\/ (ElementUp#=1)).
+(ElementUp == 1,
+(ElementDown #=1  #\/ ElementLeft#=1));
+(ElementDown == 1,
+(ElementUp #=1  #\/ ElementLeft#=1));
+(ElementLeft==1,
+(ElementDown #=1  #\/ ElementUp#=1)),
+if_then_else(ElementLeft==1,R is PositionLeft,if_then_else(ElementDown==1,R is PositionDown,R is PositionUp)).
+
 
 %Central positions
-valuePosition(Position,List,Size):-
+valuePosition(Position,List,Size,R):-
 Position>Size,
 Dim is Size*Size,
 Value is Dim-Size,
@@ -108,29 +125,43 @@ PositionRight is Position+1,
 PositionLeft is Position-1,
 PositionDown is Position+Size,
 PositionUp is Position-Size,
-nth1(Position,List,Element),
 nth1(PositionRight,List,ElementRight),
 nth1(PositionLeft,List,ElementLeft),
 nth1(PositionDown,List,ElementDown),
 nth1(PositionUp,List,ElementUp),
-Element#=1  #/\((ElementRight#=1) #\/ (ElementUp#=1) #\/ (ElementDown#=1) #\/ (ElementLeft#=1)).
+(ElementUp == 1,
+(ElementDown #=1  #\/ ElementRight#=1 #\/ ElementLeft#=1));
+(ElementDown == 1,
+(ElementUp #=1  #\/ ElementRight#=1 #\/ ElementLeft#=1));
+(ElementRight==1,
+(ElementDown #=1  #\/ ElementUp#=1 #\/ ElementLeft#=1));
+(ElementLeft==1,
+(ElementDown #=1  #\/ ElementUp#=1 #\/ ElementRight#=1)),
+if_then_else(ElementUp==1,R is PositionUp,if_then_else(ElementDown==1,R is PositionDown,
+  if_then_else(ElementLeft==1,R is PositionLeft,R is PositionRight))).
 
 %Up extreme
-valuePosition(Position,List,Size):-
+valuePosition(Position,List,Size,R):-
 Position<Size,
 Position\=Size,
 Position\=1,
 PositionRight is Position+1,
 PositionLeft is Position-1,
 PositionDown is Position+Size,
-nth1(Position,List,Element),
 nth1(PositionRight,List,ElementRight),
 nth1(PositionLeft,List,ElementLeft),
 nth1(PositionDown,List,ElementDown),
-Element#=1  #/\ ((ElementRight#=1) #\/ (ElementDown#=1) #\/ (ElementLeft#=1)).
+(ElementRight==1,
+(ElementDown #=1 #\/ ElementLeft#=1));
+(ElementLeft==1,
+(ElementDown #=1 #\/ ElementRight#=1));
+(ElementDown==1,
+(ElementRight #=1 #\/ ElementLeft#=1)),
+if_then_else(ElementRight==1,R is PositionRight,if_then_else(ElementDown==1,R is PositionDown,R is PositionLeft)).
+
 
 %Down extreme
-valuePosition(Position,List,Size):-
+valuePosition(Position,List,Size,R):-
 Dim is Size*Size,
 Value is Dim-Size,
 Position>Size,
@@ -139,8 +170,13 @@ Position<Dim,
 PositionRight is Position+1,
 PositionLeft is Position-1,
 PositionUp is Position-Size,
-nth1(Position,List,Element),
 nth1(PositionRight,List,ElementRight),
 nth1(PositionLeft,List,ElementLeft),
 nth1(PositionUp,List,ElementUp),
-Element#=1 #/\ ((ElementRight#=1) #\/ (ElementUp#=1) #\/ (ElementLeft#=1)).
+(ElementRight==1,
+(ElementUp #=1 #\/ ElementLeft#=1));
+(ElementLeft==1,
+(ElementUp #=1 #\/ ElementRight#=1));
+(ElementUp==1,
+(ElementRight #=1 #\/ ElementLeft#=1)),
+if_then_else(ElementRight==1,R is PositionRight,if_then_else(ElementLeft==1,R is PositionLeft,PositionUp)).
